0d40df3f46bfff1a8d111b4d01b6ae8990a6d4c7
Wallace Neto <wallace.neto@outlook.com>
2021-01-13 12:01:27 -0300

    Adicionando Drawer

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,23 @@
 import 'package:flutter/material.dart';
-import 'package:interLibras/app/app_module.dart';
-import 'package:flutter_modular/flutter_modular.dart';
 
-void main() => runApp(ModularApp(module: AppModule()));
+import 'components/drawer/menu.dart';
+
+void main() {
+  runApp(
+    MaterialApp(
+      home: HomePage(),
+    ),
+  );
+}
+
+class HomePage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Home'),
+      ),
+      endDrawer: MenuDrawer(),
+    );
+  }
+}
